73d36907d18005d7e64ba2a72e469b63733953be
nixinsider <nixinsider@gmail.com>
2025-11-23 03:11:18 +0500

    Add initial HTML structure, CSS styles, and smooth scrolling functionality

---
+++ NomanSaeed/script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling
+$("a.nav-link").on("click", function (e) {
+    if (this.hash !== "") {
+        e.preventDefault();
+        const hash = this.hash;
+
+        $("html, body").animate(
+            { scrollTop: $(hash).offset().top - 70 },
+            800
+        );
+    }
+});
